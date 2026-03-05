4b1359a74ac6727af554b41e707c6342ebb43f33
AliRazaDeveloper75 <alirazadeveloper75@gmail.com>
2026-03-05 16:05:18 +0500

    about u

---
+++ script.js
@@ -0,0 +1,21 @@
+// test file
+console.log("this is my js file.");
+
+// data
+
+let a = "ali"; //string
+var c = true; // boolean
+const b = 12321; //intiger
+const d = null; //null
+const e = 9007199254740992n; //big int
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(c, typeof c);
+console.log(c, typeof c);
+
+// variable
+const FirstName = "Ali ";
+let LastName = "Raza ";
+var LastName = "Raza ";
